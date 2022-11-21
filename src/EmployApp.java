04b0991a24d2756aa84bd0ec838738842fc52561
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-21 15:18:48 +0530

    second commit

--- src/EmployApp.java
+++ src/EmployApp.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class EmployApp {
+    public static void main(String[] args) {
+        int ex;
+    do {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Choice: 1.Add Employee 2.View Employee 3.Search Employee 4.Delete Employee 5.Exit");
+        int option=sc.nextInt();
+        switch (option)
+        {
+
+        }
+
+
+
+        System.out.println("Do you Want to continue?1.yes 2.No");
+        ex=sc.nextInt();
+    } while(ex==1);
+    }
 }
